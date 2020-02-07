1f05035ed920f4e589b42db8611b45585e78ac1b
truhin-andrei <truhin-andrei@bk.ru>
2020-02-08 00:34:34 +0700

    init: start task

---
+++ calculator/webpack.config.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+function NothingPlugin() {
+  this.apply = function() {};
+}
+
+const config = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    //env && env.analyze ? new BundleAnalyzerPlugin() : new NothingPlugin(),
+   // new BundleAnalyzerPlugin()
+  ],
+};
+
+module.exports = config;
